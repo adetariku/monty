712a78be69a39f629e4266dd1ff391446ad32ff6
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-09 04:54:43 +0000

    fix display

--- display.c
+++ display.c
@@ -1,4 +1,8 @@
+#define _POSIX_C_SOURCE  200809L
+#define _GNU_SOURCE
+#include <ctype.h>
 #include "monty.h"
+
 /**
  * pall - displays all values in a `stack'
  * @head: double pointer to head of stack
@@ -9,6 +13,9 @@
 void _pall(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
+
+	if (*head == NULL)
+		return;
 
 	(void)(line_number);
 	temp = *head;
